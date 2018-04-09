420c52ca56cef9a2f8c2feb3cdedf2f7a0ad5ded
yxu49 <yxu49@ucsc.edu>
2018-04-08 17:47:58 -0700

    recover

--- Lab1/pintos/src/devices/timer.c
+++ Lab1/pintos/src/devices/timer.c
@@ -100,12 +100,15 @@ void timer_sleep(int64_t ticks)
     return;
   }
   int64_t start = timer_ticks();
-  struct thread *current_thread = thread_current();
-  current_thread->time_wakeup = ticks + start;
-  intr_disable();
+  ASSERT(intr_get_level() == INTR_ON);
+  while (timer_elapsed(start) < ticks)
+    thread_yield();
+  // struct thread *current_thread = thread_current();
+  // current_thread->time_wakeup = ticks + start;
+  // intr_disable();
 
-  thread_block();
-  intr_enable();
+  // thread_block();
+  // intr_enable();
 }
 
 /* 
